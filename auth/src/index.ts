04fda94a8e040518b66666530b6240dd16935309
sandesh ghoti <sandesh.ghoti@gmail.com>
2024-06-16 13:14:52 +0000

    final changes for cluster run - now our cluster run on GKE and we can hit at Loadbalancer Ip address provide by gcp

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,8 +5,9 @@ const app = express();
 app.use(json());
 
 app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hello user!");
+  res.send("Hi there!");
 });
+
 app.listen(3000, () => {
-  console.log("Listening on port 3000!!!!");
+  console.log("Listening on port 3000!!!!!!!!");
 });
