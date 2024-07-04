5d1be12617c2b788d1d56852e625d3e435fe5dff
sandesh ghoti <sandesh.ghoti@gmail.com>
2024-07-04 10:28:35 +0000

    correct routes for auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,19 @@
 import express from "express";
 import { json } from "body-parser";
+import "express-async-errors";
+import routes from "./routes";
+import { errorHandler, NotFoundError } from "tickets-commonutils";
 
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there!");
+app.use("/api/users", routes);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
 });
 
+app.use(errorHandler);
 app.listen(3000, () => {
   console.log("Listening on port 3000!!!!!!!!");
 });
