b8f5b7f62e4938c11d7bb72e30c9721b6857fb35
sandesh ghoti <sandesh.ghoti@gmail.com>
2024-07-06 10:07:06 +0000

    refactor app to use it with supertest

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,31 +1,15 @@
-import express from "express";
-import { json } from "body-parser";
-import "express-async-errors";
-import routes from "./routes";
-import { errorHandler, NotFoundError } from "tickets-commonutils";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
+import { app } from "./app";
 import dotenv from "dotenv"; //this only need in case of .env file means without k8s depl
 dotenv.config();
-const app = express();
-app.set("trust proxy", true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false, //disable encryption: (To be understood between diff languages!) / (JWT is already encrypted)
-    secure: process.env.NODE_ENV !== "test", //True in PROD (only used with https)  //False in TEST (To work without https)
-    //RQ: NODE_ENV variable are : development | production | test
-  })
-);
 
-app.use("/api/users", routes);
-
-app.all("*", async (req, res) => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
-app.listen(3000, async () => {
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
   try {
     console.log("Starting up .....");
     if (!process.env.JWT_KEY) {
@@ -37,9 +21,12 @@ app.listen(3000, async () => {
     console.log("Connecting to mongoDB", process.env.MONGO_URI!);
     await mongoose.connect(process.env.MONGO_URI!);
     console.log(await mongoose.connection.db.listCollections().toArray());
-    console.log("Listening on port 3000!!!!!!!!");
   } catch (err) {
     console.log(err);
     process.exit(1);
   }
-});
+  app.listen(3000, async () => {
+    console.log("Listening on port 3000");
+  });
+};
+start();
