712f00a6ab5360db3dd211c457ee6822b391327b
sandesh ghoti <sandesh.ghoti@gmail.com>
2024-07-06 10:07:06 +0000

    refactor app to use it with supertest

---
+++ auth/src/app.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import { json } from "body-parser";
+import "express-async-errors";
+import { errorHandler, NotFoundError } from "tickets-commonutils";
+import cookieSession from "cookie-session";
+import routes from "./routes";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, //disable encryption: (To be understood between diff languages!) / (JWT is already encrypted)
+    secure: process.env.NODE_ENV !== "test", //True in PROD (only used with https)  //False in TEST (To work without https)
+    //RQ: NODE_ENV variable are : development | production | test
+  })
+);
+
+app.use("/api/users", routes);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+export { app };
