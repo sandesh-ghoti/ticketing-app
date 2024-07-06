4536d623e0ba241e428085f45e595356c3dcdd33
sandesh ghoti <sandesh.ghoti@gmail.com>
2024-07-06 18:41:15 +0000

    test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv"; //this only need in case of .env file means without
 dotenv.config();
 
 const start = async () => {
-  console.log("Starting up...");
+  console.log("Starting up....");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
